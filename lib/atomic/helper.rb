910c6f0a969dd32f6779ea556bc72bdb9e053655
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-22 20:39:46 -0400

    Make the implementation generic with method_missing

--- lib/atomic/helper.rb
+++ lib/atomic/helper.rb
@@ -4,18 +4,20 @@ module Atomic
       @view_context = view_context
     end
 
-    def h1(*arguments, &block)
-      if @view_context.lookup_context.template_exists?("atomic/tags/_h1")
+    def method_missing(method_name, *arguments, &block)
+      if @view_context.lookup_context.template_exists?("atomic/tags/_#{method_name}")
         options = arguments.extract_options!
 
         @view_context.render(
-          "atomic/tags/h1",
+          "atomic/tags/#{method_name}",
           arguments: arguments,
           options: options,
           block: block,
         )
+      elsif @view_context.tag.respond_to?(method_name)
+        @view_context.tag.public_send(method_name, *arguments, &block)
       else
-        @view_context.tag.h1(*arguments, &block)
+        super
       end
     end
   end
--- test/integration/atomic_test.rb
+++ test/integration/atomic_test.rb
@@ -20,14 +20,22 @@ class AtomicTest < ActiveSupport::TestCase
       <%= atomic_tag.h1 do %>
         Title From Atomic
       <% end %>
+
+      <%= atomic_tag.p do %>
+        Body From Atomic
+      <% end %>
     ERB
     declare_template "atomic/tags/_h1", <<~ERB
       <%= tag.h1(*arguments, class: "atomic-h1", **options, &block) %>
+    ERB
+    declare_template "atomic/tags/_p", <<~ERB
+      <%= tag.p(*arguments, class: "atomic-p", **options, &block) %>
     ERB
 
     render "comments/show"
 
     assert_select %(h1[class="atomic-h1"]), text: "Title From Atomic", count: 1
+    assert_select %(p[class="atomic-p"]), text: "Body From Atomic", count: 1
   end
 
   def around(&block)
