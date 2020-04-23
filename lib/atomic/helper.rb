3bfdd2257f67f2b7e4c725ab6cc668c5dc4174ed
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-22 20:36:26 -0400

    lookup template, render, otherwise fall back

--- lib/atomic/helper.rb
+++ lib/atomic/helper.rb
@@ -6,7 +6,16 @@ module Atomic
     end
 
     def h1(*arguments, **options, &block)
-      @target.h1(*arguments, **options, &block)
+      if @view_context.lookup_context.template_exists?("atomic/_h1")
+        @view_context.render(
+          "atomic/h1",
+          arguments: arguments,
+          options: options,
+          block: block,
+        )
+      else
+        @target.h1(*arguments, **options, &block)
+      end
     end
   end
 end
