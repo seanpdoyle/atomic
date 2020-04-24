2601c17c1597efa33597e50df01d5ffb2c7c9ab6
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-24 12:49:00 -0400

    extract private `Atomic::Helper#render` method

--- lib/atomic/helper.rb
+++ lib/atomic/helper.rb
@@ -16,19 +16,25 @@ module Atomic
 
     def method_missing(method_name, *arguments, &block)
       if @view_context.lookup_context.template_exists?("#{@partial_path}/_#{method_name}")
-        options = arguments.extract_options!
-
-        @view_context.render(
-          "#{@partial_path}/#{method_name}",
-          arguments: arguments,
-          options: options,
-          block: block,
-        )
+        render("#{@partial_path}/#{method_name}", *arguments, &block)
       elsif @target.respond_to?(method_name)
         @target.public_send(method_name, *arguments, &block)
       else
         super
       end
+    end
+
+    private
+
+    def render(partial_name, *arguments, &block)
+      options = arguments.extract_options!
+
+      @view_context.render(
+        partial_name,
+        arguments: arguments,
+        options: options,
+        block: block,
+      )
     end
   end
 end
