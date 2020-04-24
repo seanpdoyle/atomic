47d53b46f78b9d01245cd465d43e365f8a211c4f
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-24 13:24:43 -0400

    Add additional `atomic_tag` test coverage using `AtomicTestCase`

---
+++ test/helpers/atomic/atomic_tag_test.rb
@@ -0,0 +1,40 @@
+require "atomic_test_case"
+
+module Atomic
+  class AtomicTagTest < AtomicTestCase
+    test "falls back to Rails defaults" do
+      declare_template "comments/show", <<~ERB
+        <%= atomic_tag.h1 class: "rails-h1" do %>
+          Title From Atomic
+        <% end %>
+      ERB
+
+      render "comments/show"
+
+      assert_select %(h1[class="rails-h1"]), text: "Title From Atomic", count: 1
+    end
+
+    test "renders a template when available" do
+      declare_template "comments/show", <<~ERB
+        <%= atomic_tag.h1 do %>
+          Title From Atomic
+        <% end %>
+
+        <%= atomic_tag.p do %>
+          Body From Atomic
+        <% end %>
+      ERB
+      declare_template "atomic/tags/_h1", <<~ERB
+        <%= tag.h1(*arguments, class: "atomic-h1", **options, &block) %>
+      ERB
+      declare_template "atomic/tags/_p", <<~ERB
+        <%= tag.p(*arguments, class: "atomic-p", **options, &block) %>
+      ERB
+
+      render "comments/show"
+
+      assert_select %(h1[class="atomic-h1"]), text: "Title From Atomic", count: 1
+      assert_select %(p[class="atomic-p"]), text: "Body From Atomic", count: 1
+    end
+  end
+end
